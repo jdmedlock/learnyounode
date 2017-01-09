de9a79d2270e77d471d7c237caabb58b64cf0ad4
Jim Medlock <jdmedlock@gmail.com>
2017-01-09 15:23:41 +0000

    added httpclient.js

---
+++ httpclient.js
@@ -0,0 +1,74 @@
+// Program: filterDir.js
+// Author:  Jim Medlock
+// Date written: 1/8/2017
+
+'use strict'
+
+// Write a program that performs an HTTP GET request to a URL provided to you
+// as the first command-line argument. Write the String contents of each
+// "data" event from the response to a new line on the console (stdout).
+//
+// For this exercise you will need to use the http core module.
+// 
+// Documentation on the http module can be found by pointing your browser
+// here:
+//
+//  file:///home/ubuntu/.nvm/versions/node/v4.6.1/lib/node_modules/learnyounode/node_apidoc/http.html
+// 
+// The http.get() method is a shortcut for simple GET requests, use it to  
+// simplify your solution. The first argument to http.get() can be the URL  
+// you want to GET; provide a callback as the second argument.  
+// 
+// Unlike other callback functions, this one has the signature:  
+// 
+//  function callback (response) { /* ... */ }  
+// 
+// Where the response object is a Node Stream object. You can treat Node  
+// Streams as objects that emit events. The three events that are of most  
+// interest are: "data", "error" and "end". You listen to an event like so:  
+// 
+//  response.on("data", function (data) { /* ... */ })  
+// 
+// The "data" event is emitted when a chunk of data is available and can be  
+// processed. The size of the chunk depends upon the underlying data source.  
+// 
+// The response object / Stream that you get from http.get() also has a  
+// setEncoding() method. If you call this method with "utf8", the "data"  
+// events will emit Strings rather than the standard Node Buffer objects  
+// which you have to explicitly convert to Strings.
+
+const http = require('http');
+
+http.get(process.argv[2], (res) => {
+    const statusCode = res.statusCode;
+    const contentType = res.headers['content-type'];
+
+    let error;
+
+    if (statusCode !== 200) {
+        error = new Error(`Request Failed.\n` +
+            `Status Code: ${statusCode}`);
+    }
+    if (error) {
+        console.log(error.message);
+        // consume response data to free up memory
+        res.resume();
+        return;
+    }
+
+    res.setEncoding('utf8');
+    let rawData = [];
+    res.on('data', (chunk) => {
+        rawData.push(chunk)
+    });
+    res.on('end', () => {
+        try {
+            rawData.forEach(element => console.log(element));
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+    });
+}).on('error', (err) => {
+    console.log(`Got error: ${err.message}`);
+});
